079a339d916ad4369e08a89286b70dc35ebdd747
ContrerasKathia <kcontrerasr3@miumg.edu.gt>
2025-04-11 08:48:44 -0600

    Subiendo main

---
+++ org.grupotsi_PrototipoP3SP2025jar_1.0/src/main/java/Controlador/seguridad/Entrenador.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador.seguridad;
+
+/**
+ *
+ * @author visitante
+ */
+public class Entrenador {
+    
+    int id_perfil;
+    String nombre_perfil;
+    String estatus_perfil;
+
+    
+    public Entrenador() {
+    }
+   
+}
